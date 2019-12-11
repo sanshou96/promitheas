1b1b5a95c8820a0a5e659731de602ffef172f5b3
sanshou96 <dimitriskarslidis@hotmail.gr>
2019-12-11 18:05:57 +0200

    Update prom.js

--- scripts/prom.js
+++ scripts/prom.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $('#myNavwrap1').load('/navbar.html');
-  $('#footer').load('/footer.html');
+  $('#myNavwrap1').load('/promitheas/navbar.html');
+  $('#footer').load('/promitheas/footer.html');
+    console.log("hi");
 });
-console.log("hi");
